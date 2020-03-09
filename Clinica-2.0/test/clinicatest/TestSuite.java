ef3c806528a9b9ec44b0eb9938546eaff3627813
Vindio Pérez <vpgdkrglc@gmail.com>
2020-03-09 18:15:54 +0100

    test

--- Clinica-2.0/test/clinicatest/TestSuite.java
+++ Clinica-2.0/test/clinicatest/TestSuite.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package clinicatest;
 
 import org.junit.After;
 import org.junit.AfterClass;
